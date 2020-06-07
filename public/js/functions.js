a550d4551151322ccae83c885bd48c5534d42824
blockchainUni <batch15.017@hotmail.com>
2020-06-08 00:14:10 +0500

    changes

--- public/js/functions.js
+++ public/js/functions.js
@@ -228,6 +228,7 @@ creationTime += 86400000 //adding 70secs
       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      
         
       // Output the result in an element with id="demo"
       document.getElementById("timmer").innerHTML = hours + "h "
@@ -300,7 +301,7 @@ console.log(1)
 .then(function(receipt){
    
   stopSpinner();
-  alert('Registeration Complete')
+  alert('Registered Successfully')
   window.location.replace('/login');
   
 });
