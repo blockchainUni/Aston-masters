44f8cfe17b2b7cf2f4a5f7d52c5337ddbfc6fd87
blockchainUni <batch15.017@hotmail.com>
2020-06-08 22:07:56 +0500

    changes

--- app.js
+++ app.js
@@ -24,6 +24,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
+app.use (function (req, res, next) {
+  if (req.secure) {
+          // request was via https, so do no special handling
+          next();
+  } else {
+          // request was via http, so redirect to https
+          res.redirect('https://' + req.headers.host + req.url);
+  }
+});
+
 
 
 
@@ -66,15 +76,6 @@ app.use('/transactions',transactions);
 // catch 404 and forward to error handler
 
 
-app.use (function (req, res, next) {
-  if (req.secure) {
-          // request was via https, so do no special handling
-          next();
-  } else {
-          // request was via http, so redirect to https
-          res.redirect('https://' + req.headers.host + req.url);
-  }
-});
 
 
 app.use(function(req, res, next) {
